fced970714c984d03a4c86fafd43cba4afed3a68
kamlendra <kamlendra.chauhan@gmail.com>
2017-06-03 21:35:46 +0530

    Controller

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -2,27 +2,10 @@ var express = require('express');
 
 var routes = function(Book){
 var bookRouter = express.Router();
-
+var bookController = require('./Controllers/bookController')(Book);
 bookRouter.route('/')
-    .post(function(req,res){
-        var book = new Book(req.body);
-        book.save();
-        console.log(book);
-        res.status(201).send(book);
-    })
-    .get(function(req,res){
-        var query = {};
-        if(req.query.genre)
-        {
-            query.genre = req.query.genre;
-        }
-        Book.find(query,function(err,books){
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(books);
-        });
-    });
+    .post(bookController.post)
+    .get(bookController.get);
 
 //middleware for find book 
 bookRouter.use('/:bookId',function(req,res,next){
