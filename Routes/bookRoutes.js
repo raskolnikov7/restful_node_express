7a5f3570cd99e893045c33dfa636c28595c230f0
kamlendra <kamlendra.chauhan@gmail.com>
2017-06-03 16:54:36 +0530

    middleware

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -24,37 +24,38 @@ bookRouter.route('/')
         });
     });
 
+//middleware for find book 
+bookRouter.use('/:bookId',function(req,res,next){
+    Book.findById(req.params.bookId,function(err,book){
+            if(err)
+                res.status(500).send(err);
+            else if(book)
+            {
+                req.book = book;
+                next();
 
-bookRouter.route('/:bookId')
-.get(function(req,res){
-        //var responseJson = {hello: "This is my api"};
-        var query = {};
-        if(req.query.genre)
-        {
-            query.genre = req.query.genre;
-        }
-        Book.findById(req.params.bookId,function(err,book){
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(book);
-        });
-        //res.json(responseJson);
-    })
-    .put(function(req,res){
-        Book.findById(req.params.bookId,function(err,book){
-            if(err)
-                res.status(500).send(err);
+            }
             else
             {
-                book.title = req.body.title;
-                book.author = req.body.author;
-                book.genre = req.body.genre;
-                book.read = req.body.read;
-                book.save();
-                res.json(book);
+                res.status(404).send('No book found!');
+
             }
         });
+});
+
+
+bookRouter.route('/:bookId')
+    .get(function(req,res){
+        res.json(req.book);
+    })
+    .put(function(req,res){
+        
+        req.book.title = req.body.title;
+        req.book.author = req.body.author;
+        req.book.genre = req.body.genre;
+        req.book.read = req.body.read;
+        req.book.save();
+        res.json(req.book);
     });
 return bookRouter;
 };
