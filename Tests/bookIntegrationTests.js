e92249fa1cabd62c08f0c234a457ebb2cc60449c
kamlendra <kamlendra.chauhan@gmail.com>
2017-06-04 21:20:26 +0530

    Integration test

---
+++ Tests/bookIntegrationTests.js
@@ -0,0 +1,25 @@
+var should = require('should'),
+    request = require('supertest'),
+    app = ('../app.js'),
+    mongoose = require('mongoose'),
+    Book = mongoose.model('Book'),
+    agent = request.agent(app);
+
+describe('Book Crud Test',function(){
+    it('Should allow a book to be posted and return a read and _id',function(done){
+        var bookPost = {title:'new Book',author:'Jon',genre:'Fiction'};
+
+        agent.post('/api/books')
+            .send(bookPost)
+            .expeect(200)
+            .end(function(err,results){
+                results.body.read.should.equal(false);
+                results.body.should.have.property('_id');
+                done();
+            })
+    })
+    afterEach(function(done){
+        Book.remove().exec();
+        done();
+    })
+})
